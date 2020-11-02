2af04d75c1a1910676c291ce505bb267446abc1d
MatheusFidelis <matheusfidelis@yahoo.com>
2020-11-02 16:35:16 -0300

    Attt

--- Academia/FrmPrincipal.Designer.cs
+++ Academia/FrmPrincipal.Designer.cs
@@ -29,14 +29,34 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.cmdInserir = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.cmdInserir);
             this.panel1.Location = new System.Drawing.Point(0, 63);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1053, 100);
             this.panel1.TabIndex = 0;
+            // 
+            // cmdInserir
+            // 
+            this.cmdInserir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            this.cmdInserir.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.cmdInserir.FlatAppearance.BorderSize = 0;
+            this.cmdInserir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.cmdInserir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(46)))), ((int)(((byte)(59)))));
+            this.cmdInserir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdInserir.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdInserir.ForeColor = System.Drawing.Color.White;
+            this.cmdInserir.Location = new System.Drawing.Point(211, 12);
+            this.cmdInserir.Name = "cmdInserir";
+            this.cmdInserir.Size = new System.Drawing.Size(147, 72);
+            this.cmdInserir.TabIndex = 19;
+            this.cmdInserir.Text = "Inserir";
+            this.cmdInserir.UseVisualStyleBackColor = false;
             // 
             // FrmPrincipal
             // 
@@ -48,6 +68,7 @@
             this.Text = "InfoPhysical";
             this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
             this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -55,6 +76,7 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button cmdInserir;
     }
 }
 
